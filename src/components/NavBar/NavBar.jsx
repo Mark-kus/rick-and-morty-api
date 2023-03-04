2cca283edc85e296732466eeb9b64ba3ba0d1494
Mark-kus <tignanellimarco@gmail.com>
2023-03-04 19:16:21 -0300

    al fin funciona el integration

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,21 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import styles from './NavBar.module.css'
 import SearchBar from "../SearchBar/SearchBar";
+import { useDispatch } from "react-redux";
+import { orderCards, filterCards } from '../../redux/actions.js'
 
 export default function NavBar(props) {
+    const location = useLocation();
+    const dispatch = useDispatch();
+
+    const handleOrder = (e) => {
+        dispatch(orderCards(e.target.value))
+    }
+
+    const handleFilter = (e) => {
+        dispatch(filterCards(e.target.value))
+    }
+
     return (
         <div id={styles.navBar}>
             <div className={styles.navLinks}>
@@ -16,10 +29,24 @@ export default function NavBar(props) {
                     <button className={`${styles.linksFavorites} ${styles.links}`}>FAVORITES</button>
                 </NavLink>
             </div>
-            <div className={styles.searchBar}>
-                <SearchBar onSearch={props.onSearch} />
-                <button id={styles.random} onClick={props.onSearchRandom}>Randomize</button>
-            </div>
+            {location.pathname === '/favorites' ?
+                <div>
+                    <select name="order" onChange={handleOrder} >
+                        <option value="Ascendente">Ascendente</option>
+                        <option value="Descendente">Descendente</option>
+                    </select>
+
+                    <select name="filter" onChange={handleFilter} >
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Genderless">Genderless</option>
+                        <option value="Unknown">Unknown</option>
+                    </select>
+                </div>
+                : <div className={styles.searchBar}>
+                    <SearchBar onSearch={props.onSearch} />
+                    <button id={styles.random} onClick={props.onSearchRandom}>Randomize</button>
+                </div>}
         </div>
     )
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_FAVORITE, DELETE_FAVORITE } from "./types";
+import { ADD_FAVORITE, DELETE_FAVORITE, ORDER, FILTER } from "./types";
 
 
 const initialState = {
@@ -8,21 +8,51 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        
+
         case ADD_FAVORITE:
-            const newFavs = [...state.myFavorites];
+            const newFavs = [...state.allCharacters];
             newFavs.push(action.payload);
             return {
                 ...state,
-                myFavorites: newFavs,
+                myFavorites: state.allCharacters,
+                allCharacters: newFavs,
             }
+
+        // all fine
+        // case ADD_FAVORITE:
+        //     const newFavs = [...state.myFavorites];
+        //     newFavs.push(action.payload);
+        //     return {
+        //         ...state,
+        //         myFavorites: newFavs,
+        //     }
 
         case DELETE_FAVORITE:
             return {
                 ...state,
                 myFavorites: state.myFavorites.filter(el => el.id !== action.payload),
             }
-    
+
+        // ORDER y FILTER, casos after all fine
+
+        case ORDER:
+            if (action.payload === 'Ascendente') {
+                state.allCharacters.sort((a, b) => a.id < b.id)
+            }
+            if (action.payload === 'Descendente') {
+                state.allCharacters.sort((a, b) => a.id > b.id)
+            }
+            return {
+                ...state,
+                myFavorites: state.allCharacters,
+            }
+
+        case FILTER:
+            return {
+                ...state,
+                myFavorites: state.allCharacters.filter(el => el.gender === action.payload)
+            }
+
         default:
             return state;
     }
