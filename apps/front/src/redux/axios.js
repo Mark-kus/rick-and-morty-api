fee4594c9f2b04cecf82772174e2ff9242151e2c
Marco Tignanelli <marco_tignanelli@epam.com>
2024-11-01 16:12:56 -0300

    update url of backend

--- apps/front/src/redux/axios.js
+++ apps/front/src/redux/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
